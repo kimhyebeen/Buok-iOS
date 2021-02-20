427d2a9a9046b464599b87470067253a5e375002
denny.k <denny.k@kakaocorp.com>
2021-02-20 17:59:55 +0900

    [YITEAM-2] Pod 설정, SnapKit 추가 후 샘플 뷰 생성

--- YappHero/YappHero/ViewController.swift
+++ YappHero/YappHero/ViewController.swift
@@ -5,15 +5,25 @@
 //  Created by Denny on 2021/02/20.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    private var helloLabel: UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(helloLabel)
+        helloLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        helloLabel.text = "Hello World"
+        helloLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        helloLabel.textColor = .black
     }
-
-
+    
 }
 
