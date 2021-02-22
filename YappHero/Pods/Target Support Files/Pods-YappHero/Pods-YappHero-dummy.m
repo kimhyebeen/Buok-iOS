6d3c3360f2331a15a3c966b34dc945ae6466a402
denny.k <denny.k@kakaocorp.com>
2021-02-23 07:52:20 +0900

    [YITEAM-2] Cocoapods Stage 안된 파일 모두 추가작업

---
+++ YappHero/Pods/Target Support Files/Pods-YappHero/Pods-YappHero-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YappHero : NSObject
+@end
+@implementation PodsDummy_Pods_YappHero
+@end
---
+++ YappHero/Pods/Target Support Files/Pods-YappHeroTests/Pods-YappHeroTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YappHeroTests : NSObject
+@end
+@implementation PodsDummy_Pods_YappHeroTests
+@end
