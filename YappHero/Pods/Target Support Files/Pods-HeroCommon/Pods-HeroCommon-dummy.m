72a7c28118afd28254f84e640a34e0d1e6333cfd
denny.k <denny.k@kakaocorp.com>
2021-02-28 14:01:12 +0900

    [YITEAM-2] 불필요한 파일 모두 제거

--- YappHero/Pods/Target Support Files/Pods-HeroCommon/Pods-HeroCommon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HeroCommon : NSObject
-@end
-@implementation PodsDummy_Pods_HeroCommon
-@end
--- YappHero/Pods/Target Support Files/Pods-YappHero/Pods-YappHero-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YappHero : NSObject
-@end
-@implementation PodsDummy_Pods_YappHero
-@end
