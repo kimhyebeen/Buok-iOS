5d36234b4fab5c7e6f3077f887ac50c076d96038
denny.k <denny.k@kakaocorp.com>
2021-03-06 23:35:42 +0900

    [YITEAM-12] Center Action Button Tab Bar Controller 샘플 구현

--- BucketList/ViewControllers/Main/MainTabBarViewController.swift
+++ BucketList/ViewControllers/Main/MainTabBarViewController.swift
@@ -11,6 +11,29 @@ import HeroUI
 import SnapKit
 
 public class MainTabBarViewController: UITabBarController, UITabBarControllerDelegate {
+    // MARK: Center Action Button
+    private struct Constants {
+        static let actionButtonSize = CGSize(width: 64, height: 64)
+    }
+    
+    private let actionButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.backgroundColor = .heroBlue100s
+        button.layer.cornerRadius = Constants.actionButtonSize.height / 2
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.setImage(UIImage(named: "tab_home.png")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.imageView?.tintColor = .heroWhite100s
+        
+        button.addTarget(self, action: #selector(actionButtonTapped(sender:)), for: .touchUpInside)
+        
+        return button
+    }()
+    
     private let titleView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 24))
     private let titleLabel: UILabel = UILabel()
     
@@ -33,7 +56,10 @@ public class MainTabBarViewController: UITabBarController, UITabBarControllerDel
         titleLabel.textColor = .black
         titleLabel.text = "타이틀"
         
+        view.addSubview(actionButton)
+        
         setupTabBarItems()
+        setupActionButtonConstraints()
     }
     
     private func setupTabBarItems() {
@@ -43,8 +69,14 @@ public class MainTabBarViewController: UITabBarController, UITabBarControllerDel
         homeVC.tabBarItem = homeItem
         viewControllerList.append(homeVC)
         
+        let secondVC = SecondViewController()
+        let secondItem = UITabBarItem(title: "", image: UIImage(named: "tab_home_un.png"), selectedImage: UIImage(named: "tab_home.png"))
+        secondVC.tabBarItem = secondItem
+        viewControllerList.append(secondVC)
+        
         self.tabBar.barTintColor = .white
         self.tabBar.tintColor = .heroBlue100s
+        
         
         self.viewControllers = viewControllerList
     }
@@ -55,7 +87,6 @@ public class MainTabBarViewController: UITabBarController, UITabBarControllerDel
         switch tabBarController.selectedIndex {
         case 0:
             titleLabel.text = "타이틀"
-            break
         case 1:
             break
         case 2:
@@ -64,6 +95,19 @@ public class MainTabBarViewController: UITabBarController, UITabBarControllerDel
             break
         default:
             break
+        }
+    }
+    
+    @objc private func actionButtonTapped(sender: UIButton) {
+        DebugLog("Action Button Tapped")
+    }
+    
+    private func setupActionButtonConstraints() {
+        actionButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(Constants.actionButtonSize.width)
+            make.height.equalTo(Constants.actionButtonSize.height)
+            make.bottom.equalTo(tabBar.safeAreaLayoutGuide.snp.bottom)
         }
     }
 }
---
+++ BucketList/ViewControllers/Main/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BucketList
+//
+//  Created by denny on 2021/03/06.
+//
+
+import Foundation
+import HeroUI
+import SnapKit
+
+public class SecondViewController: HeroBaseViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
