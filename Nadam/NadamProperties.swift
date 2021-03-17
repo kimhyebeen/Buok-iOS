d420e006972adcdafc5f0d8672eea46618770883
denny.k <denny.k@kakaocorp.com>
2021-03-17 20:24:33 +0900

    [YITEAM-24] NadamProperties 기본 틀 구성

--- Nadam/NadamProperties.swift
+++ Nadam/NadamProperties.swift
@@ -7,7 +7,43 @@
 
 import Foundation
 import HeroCommon
+import HeroUI
 
-public class NadamProperties {
+public enum NadamUserStatus: Int {
+    case guest = 0
+    case normal
+    case power
+    case admin
+}
+
+public class NadamProperties: NSObject {
+    private struct KeyConstants {
+        static let isPasswordLock = "isPasswordLock"
+        static let orientation = "orientation"
+    }
     
+    static var shared = NadamProperties()
+    
+    deinit {
+        DebugLog("NadamProperties deinit")
+    }
+    
+    func resetCommonProperties() {
+        
+    }
+    
+    lazy var model: String = {
+        let model: String = UIDevice.current.model
+        return model
+    }()
+    
+    lazy var userAgent: String = {
+        let userAgent = "KT \(appVersion)"
+        return userAgent
+    }()
+    
+    lazy var appVersion: String = {
+        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "N/A"
+        return appVersion
+    }()
 }
