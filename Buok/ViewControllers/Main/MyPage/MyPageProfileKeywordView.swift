dd4344418e351668c278dc96cc4fa74f58a53aa3
denny.k <denny.k@kakaocorp.com>
2021-04-10 18:27:12 +0900

    [YITEAM-36] Keyword View 추가

---
+++ Buok/ViewControllers/Main/MyPage/MyPageProfileKeywordView.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageProfileKeywordView.swift
+//  Buok
+//
+//  Created by Taein Kim on 2021/04/10.
+//
+
+import Foundation
+import HeroCommon
+import HeroUI
+
+final class MyPageProfileKeywordView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .heroGray600s
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
