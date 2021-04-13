6bc3debf10c16cf31457703bad344f4f3ac03d11
kimhyebeen <kimhbin@naver.com>
2021-04-13 20:43:49 +0900

    [YITEAM-35] 로그인화면에서 공통으로 사용하는 SignServiceButton 구현

---
+++ HeroUI/HeroUI/ServiceButton/SignServiceButton.swift
@@ -0,0 +1,36 @@
+//
+//  ServiceButton.swift
+//  HeroUI
+//
+//  Created by 김혜빈 on 2021/04/13.
+//
+
+import UIKit
+
+public class SignServiceButton: UIButton {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.backgroundColor = .heroServiceNavy
+        self.layer.cornerRadius = 8
+    }
+    
+    public func setHeroTitle(_ text: String) {
+        self.setAttributedTitle(NSAttributedString(string: text, attributes: [.font: UIFont.font17P, .foregroundColor: UIColor.white]), for: .normal)
+        self.setAttributedTitle(NSAttributedString(string: text, attributes: [.font: UIFont.font17P, .foregroundColor: UIColor.heroDisableDarkGray]), for: .disabled)
+    }
+    
+    public func setHeroEnable(_ value: Bool) {
+        self.isEnabled = value
+        self.backgroundColor = value ? .heroServiceNavy : .heroDisableGray
+    }
+
+}
