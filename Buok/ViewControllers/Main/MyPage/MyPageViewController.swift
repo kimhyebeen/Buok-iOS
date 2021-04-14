dbae9ea28758d82d4d9f024e4fbd6584aa1b3d3a
denny.k <denny.k@kakaocorp.com>
2021-04-14 12:00:10 +0900

    [YITEAM-43] MyPage 멀티 스크롤 처리 완료

--- Buok/ViewControllers/Main/MyPage/MyPageViewController.swift
+++ Buok/ViewControllers/Main/MyPage/MyPageViewController.swift
@@ -11,10 +11,13 @@ import HeroUI
 import SnapKit
 
 public class MyPageViewController: HeroBaseViewController {
-    private let scrollView: UIScrollView = UIScrollView()
-    private let contentView: UIView = UIView()
+    private var collectionView: UICollectionView!
     
     private let profileView: MyPageProfileView = MyPageProfileView()
+    private let mediumForFixPosition: UIView = {
+        $0.backgroundColor = .heroWhite100s
+        return $0
+    }(UIView())
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,29 +54,38 @@ public class MyPageViewController: HeroBaseViewController {
     }
     
     private func setupViewLayout() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(contentView)
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionViewCell")
         
-        scrollView.snp.makeConstraints { make in
+        view.addSubview(collectionView)
+        view.addSubview(profileView)
+        view.addSubview(mediumForFixPosition)
+        
+        collectionView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.left.right.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
         
-        contentView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-            make.height.equalToSuperview().priority(250)
-            make.width.equalToSuperview()
-        }
-        
-        contentView.backgroundColor = .heroGraySample100s
-        contentView.addSubview(profileView)
+        collectionView.backgroundColor = .heroGraySample100s
         
         profileView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.left.equalToSuperview().offset(16)
             make.right.equalToSuperview().offset(-16)
         }
+        
+        mediumForFixPosition.snp.makeConstraints { make in
+            make.top.equalTo(profileView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.contentInset =
+            UIEdgeInsets(top: 244 + 16,
+                         left: 0, bottom: 0, right: 0)
     }
     
 //    @objc
@@ -91,4 +103,55 @@ public class MyPageViewController: HeroBaseViewController {
 //
 //        alertController.showAlert(title: "샘플 타이틀", message: "샘플 텍스트입니다. 샘플 텍스트입니다.", buttonSetType: .okCancel)
 //    }
+}
+
+extension MyPageViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    static let offsetHeaderStop: CGFloat = 191 + 16
+    
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let totalOffset = scrollView.contentOffset.y + profileView.bounds.height
+            + mediumForFixPosition.bounds.height
+        
+        var headerTransform = CATransform3DIdentity
+        var segmentTransform = CATransform3DIdentity
+        
+//        if totalOffset < 0 {
+//            let headerScaleFactor: CGFloat = -(totalOffset) / profileView.bounds.height
+//            let headerSizevariation = ((profileView.bounds.height * (1.0 + headerScaleFactor)) - profileView.bounds.height) / 2
+//            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+//            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+//        } else {
+            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-MyPageViewController.offsetHeaderStop, -totalOffset), 0)
+//        }
+        
+        profileView.layer.transform = headerTransform
+        
+        let segmentViewOffset = -totalOffset
+        segmentTransform = CATransform3DTranslate(segmentTransform, 0, max(segmentViewOffset, -MyPageViewController.offsetHeaderStop), 0)
+        mediumForFixPosition.layer.transform = segmentTransform
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let colors: [UIColor] = [.systemBlue, .systemGreen, .systemTeal, .systemYellow, .systemPink]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row % 5]
+        return cell
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.frame.width / 3 - 1
+        return CGSize(width: width, height: width)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
 }
