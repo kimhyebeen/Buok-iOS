6a166aabb39414dfd8a976693b12a9c33e8ca485
kimhyebeen <kimhbin@naver.com>
2021-05-22 08:24:53 +0900

    [YITEAM-76] 북마크 깃발 cell의 정보를 담을 BuokmarkFlag 구조체 생성

---
+++ Buok/Model/BuokmarkFlag.swift
@@ -0,0 +1,14 @@
+//
+//  BuokmarkFlag.swift
+//  Buok
+//
+//  Created by 김혜빈 on 2021/05/22.
+//
+
+import Foundation
+
+struct BuokmarkFlag {
+    var date: String
+    var title: String
+    var category: String
+}
