c01031791eebc7a07ec22bdbce74257038e3bcb5
kimhyebeen <kimhbin@naver.com>
2021-05-22 18:23:04 +0900

    [YITEAM-81] 친구페이지 ViewController 생성

---
+++ Buok/ViewControllers/Main/FriendPage/FriendPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FriendPageViewController.swift
+//  Buok
+//
+//  Created by 김혜빈 on 2021/05/22.
+//
+
+import HeroUI
+
+class FriendPageViewController: HeroBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+}
