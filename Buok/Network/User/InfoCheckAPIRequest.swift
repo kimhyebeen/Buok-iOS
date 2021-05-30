f81f59cca6a7bf7b67da2fa15bc4b8493da50e75
bomin <kbm97121@swu.ac.kr>
2021-05-31 01:51:58 +0900

    [YITEAM-102] InfoCheckAPIRequest 추가

---
+++ Buok/Network/User/InfoCheckAPIRequest.swift
@@ -0,0 +1,94 @@
+//
+//  InfoCheckAPIRequest.swift
+//  Buok
+//
+//  Created by 김보민 on 2021/05/31.
+//
+
+import Foundation
+import HeroCommon
+import HeroNetwork
+import Promise
+
+struct NickName: Codable {
+	var nickName: String
+}
+
+struct InfoCheckServerModel: Codable {
+	var status: Int
+	var message: String
+	var data: String?
+}
+
+public struct InfoCheckAPIRequest {
+	enum InfoCheckRequestType: APIRequestType {
+		case emailCheck(email: String)
+		case nicknameCheck(nickname: String)
+		
+		var requestURL: URL {
+			switch self {
+			case .emailCheck:
+				return URL(string: HeroConstants.user + "/email-check")!
+			case .nicknameCheck:
+				return URL(string: HeroConstants.user + "/nickname-check")!
+			}
+		}
+		
+		var requestParameter: [String: Any]? {
+			nil
+		}
+		
+		var httpMethod: HeroRequest.Method {
+			.post
+		}
+		
+		var encoding: HeroRequest.RequestEncoding {
+			.json
+		}
+		
+		var requestBody: [String: Any]? {
+			switch self {
+			case let .emailCheck(email):
+				return ["email": email]
+			case let .nicknameCheck(nickname):
+				return ["nickname": nickname]
+			}
+		}
+	}
+	
+	static func checkEmail(email: String, responseHandler: @escaping (Result<String, HeroAPIError>) -> ()) {
+		BaseAPIRequest.requestJSONResponse(requestType: InfoCheckRequestType.emailCheck(email: email)).then { responseData in
+			do {
+				if let dictData = responseData as? NSDictionary {
+					let jsonData = try JSONSerialization.data(withJSONObject: dictData, options: .prettyPrinted)
+					let getData = try JSONDecoder().decode(InfoCheckServerModel.self, from: jsonData)
+					if getData.status < 300 {
+						responseHandler(.success(getData.data ?? ""))
+					} else {
+						responseHandler(.failure(HeroAPIError(errorCode: ErrorCode(rawValue: getData.status)!, statusCode: getData.status, errorMessage: getData.message)))
+					}
+				}
+			} catch {
+				ErrorLog("InfoCheckAPIRequest ERROR")
+			}
+		}
+	}
+	
+	static func checkNickname(nickname: String, responseHandler: @escaping (Result<Bool, HeroAPIError>) -> ()) {
+		BaseAPIRequest.requestJSONResponse(requestType: InfoCheckRequestType.nicknameCheck(nickname: nickname)).then { responseData in
+			do {
+				if let dictData = responseData as? NSDictionary {
+					let jsonData = try JSONSerialization.data(withJSONObject: dictData, options: .prettyPrinted)
+					let getData = try JSONDecoder().decode(InfoCheckServerModel.self, from: jsonData)
+					if getData.status < 300 {
+						responseHandler(.success(true))
+					} else {
+						responseHandler(.failure(HeroAPIError(errorCode: ErrorCode(rawValue: getData.status)!, statusCode: getData.status, errorMessage: getData.message)))
+					}
+				}
+			} catch {
+				ErrorLog("InfoCheckAPIRequest ERROR")
+			}
+		}
+	}
+}
