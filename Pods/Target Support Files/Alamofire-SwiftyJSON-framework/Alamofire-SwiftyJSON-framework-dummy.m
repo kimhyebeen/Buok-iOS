7342e11a84d3cd9af7b3342244b02ea5524713d8
denny.k <denny.k@kakaocorp.com>
2021-05-31 17:10:01 +0900

    Pod 업데이트

--- Pods/Target Support Files/Alamofire-SwiftyJSON-framework/Alamofire-SwiftyJSON-framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Alamofire_SwiftyJSON_framework : NSObject
-@end
-@implementation PodsDummy_Alamofire_SwiftyJSON_framework
-@end
--- Pods/Target Support Files/Alamofire-SwiftyJSON-library/Alamofire-SwiftyJSON-library-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Alamofire_SwiftyJSON_library : NSObject
-@end
-@implementation PodsDummy_Alamofire_SwiftyJSON_library
-@end
