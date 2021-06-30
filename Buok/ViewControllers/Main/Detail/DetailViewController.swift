8b98e89b981c84c569c0b5f1d0d372186f286399
denny.k <denny.k@kakaocorp.com>
2021-06-30 22:09:03 +0900

    이미지 상세 뷰 적용 (페이징 가능하도록 구현)

--- Buok/ViewControllers/Main/Detail/DetailViewController.swift
+++ Buok/ViewControllers/Main/Detail/DetailViewController.swift
@@ -519,4 +519,14 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
         
         return UICollectionViewCell()
     }
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == imageCollectionView {
+            let detailVC = ImageDetailViewController()
+            detailVC.currentPage = indexPath.row
+            detailVC.attachments = viewModel?.imageUrlList.value
+            detailVC.modalPresentationStyle = .fullScreen
+            present(detailVC, animated: true)
+        }
+    }
 }
